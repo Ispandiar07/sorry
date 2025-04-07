91dffc76159f61e2b4ef8df355d36b706077a24b
Ispandiar07 <mutalipispandiar@gmail.com>
2025-04-07 17:35:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let textIndex = 0;
 
 function typeWriter(element, text, i, delay) {
     if (i < text.length) {
-        element.innerHTML += text.charAt(i);
+        element.textContent = text.substring(0, i + 1);
         setTimeout(() => typeWriter(element, text, i + 1, delay), delay);
     } else if (textIndex < textElements.length - 1) {
         textIndex++;
@@ -15,7 +15,7 @@ function startTyping() {
     const currentElement = textElements[textIndex];
     const text = currentElement.textContent;
     currentElement.textContent = '';
-    typeWriter(currentElement, text, 0, 50); // Измени 50, чтобы изменить скорость печати
+    typeWriter(currentElement, text, 0, 50);
 }
 
 startTyping();
