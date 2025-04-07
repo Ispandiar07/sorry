b29d14c2390c6b3aa017716f0e384a93011aa22e
Ispandiar07 <mutalipispandiar@gmail.com>
2025-04-07 18:00:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,13 @@
-const textElements = document.querySelectorAll('.typing-text');
-let textIndex = 0;
+const letterText = document.querySelector('.letter-container');
+letterText.style.opacity = 0;
+let opacity = 0;
 
-function typeWriter(element, text, i, delay) {
-    if (i < text.length) {
-        element.innerHTML = text.substring(0, i + 1);
-        setTimeout(() => typeWriter(element, text, i + 1, delay), delay);
-    } else if (textIndex < textElements.length - 1) {
-        textIndex++;
-        setTimeout(() => startTyping(), delay);
+function fadeIn() {
+    opacity += 0.01;
+    letterText.style.opacity = opacity;
+    if (opacity < 1) {
+        requestAnimationFrame(fadeIn);
     }
 }
 
-function startTyping() {
-    const currentElement = textElements[textIndex];
-    const text = currentElement.textContent;
-    currentElement.textContent = '';
-    typeWriter(currentElement, text, 0, 50);
-}
-
-startTyping();
+fadeIn();
